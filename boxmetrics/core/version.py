00b1da26492cb4121a84ff147ce1d9a52972ef62
Laurent-PANEK <laurent.panek@ynov.com>
2019-03-05 14:11:50 +0100

    Init versionning scheme

--- boxmetrics/core/version.py
+++ boxmetrics/core/version.py
@@ -1,7 +1,24 @@
-
 from cement.utils.version import get_version as cement_get_version
+import re
 
-VERSION = (0, 0, 1, 'alpha', 0)
+__version__ = "0.0.1-alpha.1"
 
-def get_version(version=VERSION):
+
+def parse_version(version):
+    parsed_version = re.compile(r"\.|\-").split(version)
+
+    if len(parsed_version) == 3:
+        parsed_version.append("final")
+
+    return tuple(parsed_version)
+
+
+VERSION = parse_version(__version__)
+
+
+def get_normalize_version(version=VERSION):
     return cement_get_version(version)
+
+
+def get_version(version=__version__):
+    return version
