ea771ed81f230adebbb18831560be9daae12672e
Laurent-PANEK <laurent.panek@ynov.com>
2019-03-10 15:15:19 +0100

    add missing package_data

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ from boxmetrics.core.version import get_version
 
 VERSION = get_version()
 
-f = open('README.md', 'r')
+f = open("README.md", "r")
 LONG_DESCRIPTION = f.read()
 f.close()
 
 setup(
-    name='boxmetrics',
+    name="boxmetrics",
     version=VERSION,
-    description='CLI for Boxmetrics Application',
+    description="CLI for Boxmetrics Application",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    author='Boxmetrics Team',
-    author_email='team@boxmetrics.ml',
-    url='https://github.com/Laurent-PANEK/boxmetrics-cli',
-    license='MIT',
-    packages=find_packages(exclude=['ez_setup', 'tests*']),
-    package_data={'boxmetrics': ['templates/*']},
+    long_description_content_type="text/markdown",
+    author="Boxmetrics Team",
+    author_email="team@boxmetrics.ml",
+    url="https://github.com/Laurent-PANEK/boxmetrics-cli",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "tests*"]),
+    package_data={"boxmetrics": ["templates/*", "scripts/*"]},
     include_package_data=True,
     entry_points="""
         [console_scripts]
