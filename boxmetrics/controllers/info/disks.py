07258b11951a3a267a43063c94992756206e3672
Laurent-PANEK <laurent.panek@ynov.com>
2019-03-30 17:23:08 +0100

    add disks stats

--- boxmetrics/controllers/info/disks.py
+++ boxmetrics/controllers/info/disks.py
@@ -13,3 +13,11 @@ class Disks(Controller):
         """Default action if no sub-command is passed."""
 
         self.app.render(infoDisks.all())
+
+    @ex(help="get partitions info")
+    def partitions(self):
+        self.app.render(infoDisks.partitions())
+
+    @ex(help="get stats of disks")
+    def stats(self):
+        self.app.render(infoDisks.stats())
--- boxmetrics/core/info/disks.py
+++ boxmetrics/core/info/disks.py
@@ -17,8 +17,21 @@ class InfoDisks(object):
 
             yield part
 
-    def all(self):
+    def partitions(self):
         return list(self.__get_partition())
+
+    def stats(self):
+        system = psutil.disk_io_counters()._asdict()
+
+        perdisk = psutil.disk_io_counters(True)
+
+        for disk, info in perdisk.items():
+            perdisk[disk] = info._asdict()
+
+        return dict(total=system, disks=perdisk)
+
+    def all(self):
+        return dict(partitions=self.partitions(), stats=self.stats())
 
     def __format(self, memory_info):
         for info, data in memory_info.items():
@@ -37,6 +50,17 @@ class InfoDisks(object):
                 value = float(data) / prefix[s]
                 return "%.1f%s" % (value, s)
         return "%sB" % data
+
+    def __convert_speed(self, data):
+        symbols = ("K", "M", "G", "T", "P", "E", "Z", "Y")
+        prefix = {}
+        for i, s in enumerate(symbols):
+            prefix[s] = 1 << (i + 1) * 10
+        for s in reversed(symbols):
+            if data >= prefix[s]:
+                value = float(data) / prefix[s]
+                return "%.2f%s/s" % (value, s)
+        return "%.2f B/s" % (data)
 
 
 InfoDisksInst = InfoDisks()
