360409a92fbb98c25f05d64b748055e724f80a8a
Laurent-PANEK <laurent.panek@ynov.com>
2019-04-01 11:12:07 +0200

    test typing annotation

--- boxmetrics/core/info/base.py
+++ boxmetrics/core/info/base.py
@@ -1,11 +1,14 @@
 import datetime
+from typing import Dict, TypeVar
+
+Number = TypeVar("Number", int, float)
 
 
 class Info(object):
     def __init__(self, *args):
         super(Info, self).__init__(*args)
 
-    def _format(self, dictItem, is_speed=False):
+    def _format(self, dictItem: Dict, is_speed: bool = False) -> Dict:
         for info, data in dictItem.items():
             if is_speed:
                 dictItem[info] = self._convert_bytes_per_second(data)
@@ -14,10 +17,10 @@ class Info(object):
                     dictItem[info] = self._convert_bytes(data)
         return dictItem
 
-    def _format_date(self, timestamp):
+    def _format_date(self, timestamp: Number) -> str:
         return datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
 
-    def _convert_bytes(self, data):
+    def _convert_bytes(self, data: float) -> str:
         symbols = ("K", "M", "G", "T", "P", "E", "Z", "Y")
         prefix = {}
         for i, s in enumerate(symbols):
@@ -28,7 +31,7 @@ class Info(object):
                 return "%.2f%s" % (value, s)
         return "%.2fB" % data
 
-    def _convert_bytes_per_second(self, data):
+    def _convert_bytes_per_second(self, data: float) -> str:
         symbols = ("K", "M", "G", "T", "P", "E", "Z", "Y")
         prefix = {}
         for i, s in enumerate(symbols):
@@ -39,7 +42,7 @@ class Info(object):
                 return "%.2f %s/s" % (value, s)
         return "%.2f B/s" % (data)
 
-    def _zip_dict(self, first: dict, second: dict):
+    def _zip_dict(self, first: Dict, second: Dict) -> Dict:
         zip = dict()
         if len(first.keys()) != len(second.keys()):
             return dict()
